6b2366902c49df7fe83e44aa2f1490683559511a
unisung <vctor@naver.com>
2024-11-19 15:57:01 +0900

    a

---
+++ JavaPrj/src/AA/A.java
@@ -0,0 +1,9 @@
+package AA;
+
+public class A {
+//소스코드(A.java)에
+//생성자가 하나도 없으면
+//컴파일러가 default생성자 만들어줌	
+	A(int a){}
+	A(String a){}
+}
---
+++ JavaPrj/src/AA/AMain.java
@@ -0,0 +1,7 @@
+package AA;
+
+public class AMain {
+public static void main(String[] args) {
+A a =new A(10);	
+	}
+}
